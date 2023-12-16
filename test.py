e9b24f2d8a86abdb456cc455f1831148f3ca27d0
RR7B7 <fedotovkirill4000@gmail.com>
2023-12-16 15:45:44 +0300

    working on receiving propper responses

--- test.py
+++ test.py
@@ -7,8 +7,7 @@ df = pd.read_csv('data.csv', sep=',')
 param = 'GPA'
 plt.figure(figsize=(8, 6))
 sns.set(style="whitegrid")
-df['sex'].value_counts().plot(kind='pie', autopct='%1.1f%%', legend=False)
+df['room_type'].value_counts().plot(kind='pie', autopct='%1.1f%%', legend=False, fontsize=20)
 plt.ylabel('')
-plt.savefig(f'static/assets/img/graph_pie_sex.png')
 plt.show()
 
