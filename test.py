0952c973f11de2dc6ffc4c8f8809143d1bb106dd
KFN001 <fedotovkirill4000@gmail.com>
2023-12-16 18:38:03 +0300

    to_test: api, graphs, data

--- test.py
+++ test.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('data.csv', sep=',')
 param = 'GPA'
-for elem in ['school']:
-    plt.figure(figsize=(8, 6))
-    sns.set(style="whitegrid", font_scale=0.6)
-    sns.violinplot(x=elem, y='GPA', data=df)
-    plt.savefig(f'static/assets/img/graph_violin_{elem}.png')
-    plt.show()
+plt.figure(figsize=(14, 10))
+sns.set(style="whitegrid")
+df['room_type'].value_counts().plot(kind='pie', autopct='%1.1f%%', legend=False, fontsize=36)
+plt.ylabel('')
+plt.savefig('output.png', transparent=True)
+plt.show()
+
+
