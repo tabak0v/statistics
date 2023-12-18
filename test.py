d41d060c9993be5df7a8192abdd4674c02be2821
RR7B7 <fedotovkirill4000@gmail.com>
2023-12-18 10:38:56 +0300

    1.3

--- test.py
+++ test.py
@@ -5,12 +5,10 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('data.csv', sep=',')
 param = 'GPA'
-for factor in ["school"]:
+for factor in ["residents", "cleaner", "grade", "room_type", "sex"]:
     plt.figure(figsize=(8, 6))
     sns.set(style="whitegrid")
-    sns.set(font_scale=0.6)
     sns.violinplot(data=df, x=factor, y='GPA', cut=0)
-    plt.ylabel('')
     plt.savefig(f'static/assets/img/graph_violin_{factor}.png')
     plt.show()
 
